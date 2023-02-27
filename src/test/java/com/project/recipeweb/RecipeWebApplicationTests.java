fb4916fb3ffaf620630057b09b3b70eeeaf13495
eliza <eliza.an.popova@gmail.com>
2023-02-27 23:54:34 +0300

    first

---
+++ src/test/java/com/project/recipeweb/RecipeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.recipeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
